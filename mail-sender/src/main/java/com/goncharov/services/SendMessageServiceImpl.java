5c1a5494d0a3842c254fc30fd3c7dd650b50e091
Данил Гончаров <singlearity1@gmail.com>
2024-04-13 14:11:15 +0300

    implemented mail sender logic

---
+++ mail-sender/src/main/java/com/goncharov/services/SendMessageServiceImpl.java
@@ -0,0 +1,57 @@
+package com.goncharov.services;
+
+import com.goncharov.grpc.Email;
+import com.goncharov.grpc.SendMessageServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendMessageServiceImpl
+        extends SendMessageServiceGrpc.SendMessageServiceImplBase {
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public SendMessageServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+    @Async
+    public void sendMessage(SimpleMailMessage email) {
+        javaMailSender.send(email);
+    }
+
+    public SimpleMailMessage createMessage(String email, String token) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(email);
+        mailMessage.setSubject("Complete Registration!");
+        mailMessage.setFrom("company@gmail.com");
+        mailMessage.setText("To confirm your account, please click here : "
+                + "http://localhost:8080/confirmation?token=" + token);
+        return mailMessage;
+    }
+
+    @Async
+    @Override
+    public void sendMessage(Email.EmailRequest request, StreamObserver<Email.EmailResponse> responseObserver) {
+        try {
+            sendMessage(createMessage(request.getEmail(), request.getToken()));
+            var response = Email.EmailResponse.newBuilder()
+                    .setMessage("Sent")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (MailException e){
+            var response = Email.EmailResponse.newBuilder()
+                    .setMessage(e.getMessage())
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+
+
+    }
+}
